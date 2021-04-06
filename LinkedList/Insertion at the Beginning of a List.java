01c83590c8d7dd2b52f16631171e74c3b9657b28
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-07 00:07:07 +0530

    Update Insertion at the Beginning of a List.java

--- LinkedList/Insertion at the Beginning of a List.java
+++ LinkedList/Insertion at the Beginning of a List.java
@@ -1,2 +1,8 @@
 /*
+While inserting in the beginning, the new node will point to the first node of list, and then head will point to the new node.
+So basically two pointers needs to be manipulated. So,
+
+NEW[DATA] = INFO        // place the data in new node
+NEW[NEXT] = HEAD    // link the next of new node to head
+HEAD = NEW        // link the head to new node
 */
